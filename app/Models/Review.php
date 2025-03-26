8bdd5e8bee742ad213ab2a2eac2a9d4dc6eb30f4
satria bahari <satriaabaharii@gmail.com>
2025-03-26 23:08:17 +0700

    feat: add review menu in dashboard

---
+++ app/Models/Review.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Review extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'course_id',
+        'user_id',
+        'rating',
+        'comment',
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
