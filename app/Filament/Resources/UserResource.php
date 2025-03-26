e451ce285e7b61bedddd242876afbb5c3736671d
satria bahari <satriaabaharii@gmail.com>
2025-03-26 23:08:27 +0700

    feat: add user menu in dashboard

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Dom\Text;
+use Filament\Forms;
+use App\Models\User;
+use Filament\Tables;
+use Filament\Forms\Form;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\DateTimePicker;
+use App\Filament\Resources\UserResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\UserResource\RelationManagers;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(255)
+                    ->placeholder('Masukkan nama lengkap'),
+                TextInput::make("email")
+                    ->email()
+                    ->required()
+                    ->unique(User::class, 'email')
+                    ->placeholder('Masukkan email'),
+                TextInput::make("password")
+                    ->password()
+                    ->required()
+                    ->minLength(8)
+                    ->placeholder('Masukkan password'),
+                Select::make("role")
+                    ->options([
+                        "peserta" => "Peserta",
+                        "guru" => "Guru",
+                        "admin" => "Admin"
+                    ])
+                    ->required(),
+                DateTimePicker::make('created_at')
+                    ->label('Dibuat Pada')
+                    ->disabled(),
+                DateTimePicker::make('updated_at')
+                    ->label('Diperbarui Pada')
+                    ->disabled(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('No.')
+                    ->rowIndex(),
+                TextColumn::make("name")
+                    ->searchable(),
+                TextColumn::make("email")
+                    ->searchable(),
+                TextColumn::make("role")
+                    ->sortable(),
+                TextColumn::make("remember_token")
+                    ->hidden(),
+                TextColumn::make("created_at")
+                    ->label('Dibuat Pada')
+                    ->dateTime('d M Y, H:i')
+                    ->sortable(),
+                TextColumn::make("updated_at")
+                    ->label('Diperbarui Pada')
+                    ->dateTime('d M Y, H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
