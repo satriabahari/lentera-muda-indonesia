0a30c5e6e681c8a276ee2726e82ed36a8fd539bb
satria bahari <satriaabaharii@gmail.com>
2025-04-01 23:56:28 +0700

    refactor: change some code review

--- app/Filament/Resources/ReviewResource/Pages/CreateReview.php
+++ app/Filament/Resources/ReviewResource/Pages/CreateReview.php
@@ -9,4 +9,8 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateReview extends CreateRecord
 {
     protected static string $resource = ReviewResource::class;
+    protected function getRedirectUrl(): string
+    {
+        return $this->getResource()::getUrl('index');
+    }
 }
