3c253ce82b4feaf5d95960c2386648c198e4644e
satria bahari <satriaabaharii@gmail.com>
2025-04-03 17:47:06 +0700

    chore: remove FluxUI

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,9 +8,6 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        {{-- <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
-
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600&display=swap" rel="stylesheet" />
 
@@ -18,7 +15,7 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
 
         <!-- FluxUI -->
-        @fluxAppearance
+        {{-- @fluxAppearance --}}
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -39,7 +36,7 @@
             </main>
 
             <!-- FluxUI -->
-            @fluxScripts
+            {{-- @fluxScripts --}}
         </div>
     </body>
 </html>
