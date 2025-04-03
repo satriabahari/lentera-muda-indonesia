005fdd5eec3185514321b6330cedbe3211ff75c9
satria bahari <satriaabaharii@gmail.com>
2025-04-03 15:37:27 +0700

    refactor: change some code in quiz

--- resources/views/livewire/quiz-list.blade.php
+++ resources/views/livewire/quiz-list.blade.php
@@ -2,13 +2,14 @@
     <h2 class="text-2xl font-bold mb-4">quizzes</h2>
 
     @forelse ($quizzes as $quiz)
-        <div class="mb-4">
-            <a href="{{ route('quiz.show', ['course' => $quiz->course_id, 'quiz' => $quiz->id]) }}"
-                class='cursor-pointer bg-gray-800 px-4 py-2 rounded-md'>
-                {{ $quiz->title }}
-            </a>
-        </div>
-
+        @if ($quiz->status === 'published')
+            <div class="mb-4">
+                <a href="{{ route('quiz.show', ['course' => $quiz->course_id, 'quiz' => $quiz->id]) }}"
+                    class='cursor-pointer bg-gray-800 px-4 py-2 rounded-md'>
+                    {{ $quiz->title }}
+                </a>
+            </div>
+        @endif
     @empty
         <p>No quizzes available.</p>
     @endforelse
