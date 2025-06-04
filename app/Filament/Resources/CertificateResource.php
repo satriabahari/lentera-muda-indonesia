e13f8836ae90887a82fcd895209065bc2eecdca7
satria bahari <satriaabaharii@gmail.com>
2025-06-04 11:47:08 +0700

    feat: add certificate resource

---
+++ app/Filament/Resources/CertificateResource.php
@@ -0,0 +1,160 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CertificateResource\Pages;
+use App\Filament\Resources\CertificateResource\RelationManagers;
+use App\Models\Certificate;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Tables\Actions\ActionGroup;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Support\Colors\Color;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Columns\IconColumn;
+
+class CertificateResource extends Resource
+{
+    protected static ?string $model = Certificate::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-trophy';
+
+    protected static ?string $navigationGroup = 'Content Management';
+    protected static ?int $navigationSort = 4;
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make("name")
+                    ->required()
+                    ->maxLength(255),
+
+                Select::make('course_id')
+                    ->relationship('course', 'title')
+                    ->required()
+                    ->label('Course'),
+
+                ToggleButtons::make('is_active')
+                    ->boolean()
+                    ->inline()
+                    ->grouped()
+                    ->required(),
+
+                FileUpload::make('image')
+                    ->directory('courses')
+                    ->disk('public')
+                    ->image()
+                    ->imageEditor()
+                    ->visibility('public')
+                    ->maxSize(2048)
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('No.')
+                    ->rowIndex()
+                    ->alignCenter(),
+                TextColumn::make('name')->searchable(),
+                TextColumn::make('course.title')
+                    ->label('Course')
+                    ->searchable(),
+                ImageColumn::make('image')
+                    ->disk('public')
+                    ->visibility('public')
+                    ->defaultImageUrl(url('/images/placeholder.png'))
+                    ->square(),
+                IconColumn::make('is_active')
+                    ->boolean()
+                    ->alignCenter(),
+                TextColumn::make("created_at")
+                    ->dateTime('d M Y, H:i')
+                    ->sortable(),
+                TextColumn::make("updated_at")
+                    ->dateTime('d M Y, H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                SelectFilter::make('course_id')
+                    ->relationship('course', 'title')
+                    ->label('Course'),
+
+                SelectFilter::make("is_active")
+                    ->options([
+                        '1' => 'True',
+                        '0' => 'False',
+                    ]),
+            ],)
+            ->filtersTriggerAction(
+                fn(Action $action) => $action
+                    ->button()
+                    ->label('Filter'),
+            )
+            ->actions([
+                ActionGroup::make([
+                    Tables\Actions\ViewAction::make(),
+                    Tables\Actions\EditAction::make(),
+                    Tables\Actions\DeleteAction::make(),
+                ])
+                    ->button()
+                    ->label('Actions')
+                    ->color('primary')
+                    ->tooltip('Actions'),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Action::make('create')
+                    ->label('Create Certificate')
+                    ->url(route('filament.admin.resources.certificates.create'))
+                    ->icon('heroicon-m-plus')
+                    ->button(),
+            ])
+            ->striped();
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCertificates::route('/'),
+            'create' => Pages\CreateCertificate::route('/create'),
+            'edit' => Pages\EditCertificate::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+    public static function getNavigationBadgeTooltip(): ?string
+    {
+        return 'Total Certificates';
+    }
+}
